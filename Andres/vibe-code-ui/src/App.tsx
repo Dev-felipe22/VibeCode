a0a9eb30ef64ae49f720f1b8f6d809131e7b47b9
Andres Escamilla <ndres.f.esca98@gmail.com>
2025-06-21 16:43:35 -0500

    Merged both features

--- Andres/vibe-code-ui/src/App.tsx
+++ Andres/vibe-code-ui/src/App.tsx
@@ -1,10 +1,21 @@
 import TaskBar from "./components/TaskBar";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
   return (
-    <div style={{ paddingTop: "48px" }}>
-      <TaskBar />
-    </div>
+    <BrowserRouter>
+        <div style={{ paddingTop: "48px" }}>
+          <TaskBar />
+          <Routes>
+            <Route path="/" element={<Login />} />
+            {/* <Route path="/register" element={<Register />} /> */}
+          </Routes>
+        </div>
+    </BrowserRouter>
+
   );
 }
 
